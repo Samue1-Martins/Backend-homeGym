7aa160a0f22b012f0256f19124e7bc6f5d3f4ded
Samuel Martins <125680404+Samue1-Martins@users.noreply.github.com>
2024-04-01 15:30:58 -0300

    including routes and functions: working perfectly

--- src/database/lower.exercises.js
+++ src/database/lower.exercises.js
@@ -1,11 +1,12 @@
-export const lowerExercise = [
-    {Nome:'Agachamento', Repetições:'3x 12 repetições'}, 
-    {Nome:'Afundo', Repetições: '3x 12 repetições'}, 
-    {Nome:'Agachamento cadeirinha', Repetições:'1x 2 minutos'}, 
-    {Nome:'Stiff', Repetições: '3x 12 repetições'},
-    {Nome:'Flexão de joelho p/ trás ', Repetições:'3x 12 repetições'}, 
-    {Nome:'Abdução', Repetições: '3x 12 repetições'},
-    {Nome:'Agachamento sumô', Repetições: '3x 12 repetições'},
-    {Nome:'Elevação pélvica', Repetições: '3x 12 repetições'}
-]
+export const lowerExercise = {
+    exercises: [
+        {id: 1, Nome:'Agachamento', Repetições:'3x 12 repetições'},
+        {id: 2, Nome:'Afundo', Repetições: '3x 12 repetições'}, 
+        {id: 3, Nome:'Agachamento cadeirinha', Repetições:'1x 2 minutos'}, 
+        {id: 4, Nome:'Stiff', Repetições: '3x 12 repetições'},
+        {id: 5, Nome:'Flexão de joelho p/ trás ', Repetições:'3x 12 repetições'}, 
+        {id: 6, Nome:'Abdução', Repetições: '3x 12 repetições'},
+        {id: 7, Nome:'Agachamento sumô', Repetições: '3x 12 repetições'},
+        {id: 8, Nome:'Elevação pélvica', Repetições: '3x 12 repetições'}],
+};
 
